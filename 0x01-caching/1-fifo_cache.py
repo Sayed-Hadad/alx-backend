bd1d5ce900db5fb9ec72338dda649907c8dac827
sayyed <sayed@gmail.com>
2024-11-02 04:55:52 +0200

    Done2

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,7 +3,6 @@
 '''Task 1: FIFO caching
 '''
 
-
 from collections import OrderedDict
 from base_caching import BaseCaching
 
@@ -18,20 +17,22 @@ class FIFOCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        '''assign to the dictionary `self.cache_data` the
-           `item` value for the key `key`
+        '''Assign the `item` value for the key `key`
+           to the dictionary `self.cache_data`.
         '''
-
         if key is None or item is None:
             return
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+        if (key not in self.cache_data and
+                len(self.cache_data) >= BaseCaching.MAX_ITEMS):
             first_key, _ = self.cache_data.popitem(last=False)
             print(f"DISCARD: {first_key}")
 
         self.cache_data[key] = item
 
     def get(self, key):
-        '''return the value in `self.cache_data` linked to `key`
+        '''Return the value in `self.cache_data` linked to `key`.
         '''
+        if key is None:
+            return None
         return self.cache_data.get(key, None)

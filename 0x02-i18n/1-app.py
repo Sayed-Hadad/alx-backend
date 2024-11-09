87b60eff1c3c716a8a11e8bc07abf9147040c206
sayed <sayed@yahoo.com>
2024-11-09 04:24:49 +0200

    d

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,16 +1,25 @@
+#!/usr/bin/env python3
+'''Task 0: Basic Flask app
+'''
+
 from flask import Flask, render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
 
 class Config:
-    LANGUAGES  = ["en", "fr"]
+    '''Config class'''
+
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
+app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 
 babel = Babel(app)
+
 
 @app.route('/')
 def index():

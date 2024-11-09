efc193697c894485e80ab7164061ed1bba41431f
sayed <sayed@yahoo.com>
2024-11-09 04:00:42 +0200

    d

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,16 @@
-from flask import Flask , render_template
+#!/usr/bin/env python3
+
+"""
+This module provides a simple Hello World output.
+"""
+
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-        return render_template('0-index.html')
+    return render_template('0index.html')
 
 
 if __name__ == '__main__':
